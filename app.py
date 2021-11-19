11bbef1c670522571b851b8504d9670509829e4f
HWatanuki <watanuki.hugo@gmail.com>
2021-11-19 21:23:54 +0000

    2021.11.19-HMW-corrected flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, g
 DATABASE = "blog.db"
 SECRET_KEY = "pudim"
 
-app = Flask(__Name__)
+app = Flask(__name__)
 app.config.from_object(__name__)
 
 def conectar_bd():
@@ -12,14 +12,14 @@ def conectar_bd():
 
 @app.before_request
 def pre_requisicao():
-    g.bd = connectar_bd()
+    g.bd = conectar_bd()
 
 @app.teardown_request
 def pos_requisicao(exception):
     g.bd.close()
 
 @app.route('/')
-def exibir entradas():
+def exibir_entradas():
     sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
     cur = g.bd.execute(sql)
     entradas = []
